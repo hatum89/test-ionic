09155017bf320ca8c9c2a30a02156f2cd553ea4c
juan <juanhatumvarela@gmail.com>
2020-08-17 17:16:57 -0500

    pantalla 3 teclado terminado

--- src/app/pages/reg-num-cel/reg-num-cel.page.ts
+++ src/app/pages/reg-num-cel/reg-num-cel.page.ts
@@ -6,8 +6,10 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   templateUrl: './reg-num-cel.page.html',
   styleUrls: ['./reg-num-cel.page.scss'],
 })
-export class RegNumCelPage implements OnInit {
-  private form: FormGroup;
+export class RegNumCelPage {
+          form: FormGroup;
+          number: string = '';
+          number2: string = '';
 
   constructor( private formBuilder: FormBuilder) {
 
@@ -25,16 +27,103 @@ export class RegNumCelPage implements OnInit {
        ])]
     });
 
-
-
-
-
-  }
-  num(){
-
   }
 
-  ngOnInit() {
-  }
+    num1() {
+        if(this.form.get('firsNumPart').value.length == 3 ){
+       this.number2 += '1';
+   } else {
+       this.number += '1';
+   }
+    }
+    num2() {
+        if(this.form.get('firsNumPart').value.length == 3 ){
+            this.number2 += '2';
+        } else {
+            this.number += '2';
+        }
+    }
+    num3() {
+        if(this.form.get('firsNumPart').value.length == 3 ){
+            this.number2 += '3';
+        } else {
+            this.number += '3';
+        }
+
+        this.number += '3';
+    }
+    num4() {
+        if(this.form.get('firsNumPart').value.length == 3 ){
+            this.number2 += '4';
+        } else {
+            this.number += '4';
+        }
+
+    }
+    num5() {
+        if(this.form.get('firsNumPart').value.length == 3 ){
+            this.number2 += '5';
+        } else {
+            this.number += '5';
+        }
+
+    }
+    num6() {
+        if(this.form.get('firsNumPart').value.length == 3 ){
+            this.number2 += '6';
+        } else {
+            this.number += '6';
+        }
+
+    }
+    num7() {
+        if(this.form.get('firsNumPart').value.length == 3 ){
+            this.number2 += '7';
+        } else {
+            this.number += '7';
+        }
+
+    }
+    num8() {
+        if(this.form.get('firsNumPart').value.length == 3 ){
+            this.number2 += '8';
+        } else {
+            this.number += '8';
+        }
+
+        this.number += '8';
+    }
+    num9() {
+        if(this.form.get('firsNumPart').value.length == 3 ){
+            this.number2 += '9';
+        } else {
+            this.number += '9';
+        }
+
+    }
+    num0() {
+        if(this.form.get('firsNumPart').value.length == 3 ){
+            this.number2 += '0';
+        } else {
+            this.number += '0';
+        }
+    }
+    numEraser() {
+
+        if(this.form.get('firsNumPart').value.length == 3 ){
+            this.number2 = this.number2.slice(1);
+        }else {
+            this.number = this.number.slice(1);
+        }
+        if(this.form.get('secondNumPart').value.length == 0 && this.form.get('firsNumPart').value.length == 3) {
+            this.number = this.number.slice(1);
+        }
+    }
+    numAccess() {
+      console.log('form',this.form.get('firsNumPart').value + this.form.get('secondNumPart').value );
+     if(this.form.valid) {
+         console.log('numAccess');
+     }
+    }
 
 }
